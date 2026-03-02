7050fba1291e9d5931fee8689e0c08a43cb2f806
ambientcowboy <holysignals@protonmail.com>
2026-03-01 21:43:17 -0800

    Intialied for, try, and catch. Lines 11-23

--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
 ﻿namespace COMP003A.Assignment7;
-
+using System;
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+       List<double> expenses = new List<double>();
+       Console.WriteLine("Daily Expense Tracker");
+       Console.WriteLine();
+       for (int i = 0; i < 5; i++)
+       {
+           Console.WriteLine("Enter expense " + (i +1) + ": ");
+           try
+           {
+               string expenseInput = Console.ReadLine();
+               double expenseValue = Convert.ToDouble(expenseInput);
+               expenses.Add(expenseValue);
+           }
+           catch (FormatException)
+           {
+               Console.WriteLine("Invalid Input 0.00 was stored");
+               expenses.Add(0);
+           }
+       }
     }
 }
